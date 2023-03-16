5512c517f0a61a2f96b93178c2b9836e2efeaa7f
Guilherme Utzig <guilhermelutzig@gmail.com>
2023-03-16 11:22:19 -0300

    Pulled first cubejs query and rendered using React Chart

--- src/pages/charts.tsx
+++ src/pages/charts.tsx
@@ -2,112 +2,81 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
   Title,
   Tooltip,
   Legend,
-  ArcElement,
+  BarElement,
 } from "chart.js";
-import { Line, Pie } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
+import { Bar } from "react-chartjs-2";
+import { useCubeQuery } from "@cubejs-client/react";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
-  Legend,
-  ArcElement
+  Legend
 );
 
-export const options = {
-  responsive: true,
-  interaction: {
-    mode: "index" as const,
-    intersect: false,
-  },
-  stacked: false,
-  plugins: {
-    title: {
-      display: true,
-      text: "Chart.js Line Chart - Multi Axis",
-    },
-  },
-  scales: {
-    y: {
-      type: "linear" as const,
-      display: true,
-      position: "left" as const,
-    },
-    y1: {
-      type: "linear" as const,
-      display: true,
-      position: "right" as const,
-      grid: {
-        drawOnChartArea: false,
-      },
-    },
-  },
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-const lineData = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-      yAxisID: "y",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-      yAxisID: "y1",
-    },
-  ],
-};
-
-const pieData = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
 export default function Charts() {
+  const { resultSet, isLoading, error } = useCubeQuery({
+    measures: ["Orders.count"],
+    dimensions: ["ProductCategories.name"],
+    filters: [
+      {
+        member: "ProductCategories.name",
+        operator: "equals",
+        values: ["Beauty", "Clothing", "Computers", "Electronics"],
+      },
+    ],
+    timeDimensions: [
+      {
+        dimension: "Orders.createdAt",
+        granularity: "month",
+        dateRange: "last 6 month",
+      },
+    ],
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return <p>{error.toString()}</p>;
+  if (!resultSet) return null;
+
+  const labels = resultSet
+    .seriesNames({
+      x: [],
+      y: ["Orders.createdAt"],
+    })
+    .map(({ title }) => title);
+
+  const datasets = resultSet.series().map((item, i) => ({
+    label: item.title,
+    data: item.series.map((item) => item.value),
+  }));
+
+  console.log(labels);
+
   return (
-    <div style={{ maxWidth: 600, margin: "0 auto" }}>
-      <Line options={options} data={lineData} />
-      <hr style={{ margin: "40px 0" }} />
-      <Pie data={pieData} />
+    <div className="chart-holder">
+      <Bar
+        data={{
+          labels,
+          datasets,
+        }}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top" as const,
+            },
+            title: {
+              display: true,
+              text: "Orders",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
