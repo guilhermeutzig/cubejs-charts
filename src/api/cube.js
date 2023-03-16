25ec84657ed3361b6d6b78ef8465b3d81a9f28bb
Guilherme Utzig <guilhermelutzig@gmail.com>
2023-03-16 11:56:46 -0300

    Updated README and now using env variables

--- src/api/cube.js
+++ src/api/cube.js
@@ -1,11 +1,7 @@
 import cubejs from "@cubejs-client/core";
 
-const cubejsApi = cubejs(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTQ2NjY4OTR9.0fdi5cuDZ2t3OSrPOMoc3B1_pwhnWj4ZmM3FHEX7Aus",
-  {
-    apiUrl:
-      "https://awesome-ecom.gcp-us-central1.cubecloudapp.dev/cubejs-api/v1",
-  }
-);
+const cubejsApi = cubejs(process.env.NEXT_PUBLIC_CUBEJS_API_KEY, {
+  apiUrl: process.env.NEXT_PUBLIC_CUBEJS_API_URL,
+});
 
 export default cubejsApi;
